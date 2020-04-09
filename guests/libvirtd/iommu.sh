989d84b7bcb3cd4635a44062234bfc1b5c4aa657
Joshua Fern <JoshuaFern@ProtonMail.com>
2020-04-09 09:37:19 -0700

    delete iommu.sh

--- guests/libvirtd/iommu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-shopt -s nullglob
-for d in /sys/kernel/iommu_groups/*/devices/*; do
-    n=${d#*/iommu_groups/*}; n=${n%%/*}
-    printf 'IOMMU Group %s ' "$n"
-    lspci -nns "${d##*/}"
-done;
