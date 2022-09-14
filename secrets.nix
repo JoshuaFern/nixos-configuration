27ed9dadd560014de6a6d18187a33b9dd1b72002
Joshua Fern <joshuafern@protonmail.com>
2022-09-13 18:50:39 -0700

    Deleting .gitignore files

--- secrets.nix
+++
@@ -1,4 +0,0 @@
-{
-  accountPassword.jdf = "$6$kKUcskTCIxS$nIV8zTYv39/ItWlTwtroevDL/Asm.8Tm9yEYkNjb8.r3xgSjBfGj/VXST2H9T2F3z8ZKGay/biLasHrQnhKIt1";
-  accountPassword.root = "$6$kKUcskTCIxS$nIV8zTYv39/ItWlTwtroevDL/Asm.8Tm9yEYkNjb8.r3xgSjBfGj/VXST2H9T2F3z8ZKGay/biLasHrQnhKIt1";
-}
